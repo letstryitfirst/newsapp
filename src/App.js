a900c06de53593ebf3e3b37da3d3133bf38c1909
Prathmesh Sarwdnya <prathmeshsarwdnya@gmail.com>
2023-10-29 01:48:28 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -21,21 +21,21 @@ const [progress, setProgress] = useState(0)
         color='#f11946'
         progress={progress}/>
           <Routes>
-            <Route exact path='/' element={<News  setProgress={ setProgress} apiKey={ apiKey} key="general" pageSize= {pageSize} country="in" category="general" />}></Route>
+            <Route exact path='/newsapp/' element={<News  setProgress={ setProgress} apiKey={ apiKey} key="general" pageSize= {pageSize} country="in" category="general" />}></Route>
 
-            <Route exact path='/science' element={<News  setProgress={ setProgress} apiKey={ apiKey} key="science" pageSize= {pageSize} country="in" category="science" />}></Route>
+            <Route exact path='/newsapp/science' element={<News  setProgress={ setProgress} apiKey={ apiKey} key="science" pageSize= {pageSize} country="in" category="science" />}></Route>
 
-            <Route exact path='/business' element={<News  setProgress={ setProgress} apiKey={ apiKey}  key="business" pageSize= {pageSize} country="in" category="business" />}></Route>
+            <Route exact path='/newsapp/business' element={<News  setProgress={ setProgress} apiKey={ apiKey}  key="business" pageSize= {pageSize} country="in" category="business" />}></Route>
 
-            <Route exact path='/entertainment' element={<News  setProgress={ setProgress} apiKey={ apiKey}  key="entertainment" pageSize= {pageSize} country="in" category="entertainment" />}></Route>
+            <Route exact path='/newsapp/entertainment' element={<News  setProgress={ setProgress} apiKey={ apiKey}  key="entertainment" pageSize= {pageSize} country="in" category="entertainment" />}></Route>
 
-            <Route exact path='/health' element={<News  setProgress={ setProgress} apiKey={ apiKey} key="health" pageSize= {pageSize} country="in" category="health" />}></Route>
+            <Route exact path='/newsapp/health' element={<News  setProgress={ setProgress} apiKey={ apiKey} key="health" pageSize= {pageSize} country="in" category="health" />}></Route>
 
-            <Route exact path='/sports' element={<News  setProgress={ setProgress} apiKey={ apiKey} key="sports" pageSize= {pageSize} country="in" category="sports" />}></Route>
+            <Route exact path='/newsapp/sports' element={<News  setProgress={ setProgress} apiKey={ apiKey} key="sports" pageSize= {pageSize} country="in" category="sports" />}></Route>
 
-            <Route exact path='/technology' element={<News  setProgress={ setProgress} apiKey={ apiKey} key="technology" pageSize= {pageSize} country="in" category="technology" />}></Route>
+            <Route exact path='/newsapp/technology' element={<News  setProgress={ setProgress} apiKey={ apiKey} key="technology" pageSize= {pageSize} country="in" category="technology" />}></Route>
 
-            <Route exact path='/about' element={<About />}></Route>
+            <Route exact path='/newsapp/about' element={<About />}></Route>
           </Routes>
         </Router>
       </>
